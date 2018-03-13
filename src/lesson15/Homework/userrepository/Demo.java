a1d5dc48235e049d0b2ffdec65181f29bbe3b7a0
Kilipurt <proandrey15@gmail.com>
2018-03-13 18:02:29 +0200

    lesson15 Homework task 1

---
+++ src/lesson15/Homework/userrepository/Demo.java
@@ -0,0 +1,22 @@
+package lesson15.Homework.userrepository;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        User u1 = new User(1, "u1", "rwg2w");
+        User u2 = new User(2, "u2", "wef24");
+        User[] usAr = new User[10];
+
+        usAr[0] = u1;
+        usAr[1] = u2;
+
+        UserRepository ur = new UserRepository(usAr);
+
+        System.out.println(ur.save(u2));
+
+        ur.delete(u1);
+
+        System.out.println(Arrays.toString(usAr));
+    }
+}
