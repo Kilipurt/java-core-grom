4d012ee0bce455b3ac22a52fed3faf17b035feb8
Kilipurt <proandrey15@gmail.com>
2018-03-13 19:18:54 +0200

    lesson15 Homework task 1

--- src/lesson15/Homework/userrepository/UserRepository.java
+++ src/lesson15/Homework/userrepository/UserRepository.java
@@ -39,7 +39,7 @@ public class UserRepository {
     public User update(User user) {
         if (users != null && user != null) {
             for (int i = 0; i < users.length; i++) {
-                if (users[i] != null && user.getId() == users[i].getId() && !users[i].equals(user)) {
+                if (users[i] != null && user.getId() == users[i].getId() && !users[i].equals(user) && users[i].hashCode() != user.hashCode()) {
                     users[i] = user;
                     return users[i];
                 }
