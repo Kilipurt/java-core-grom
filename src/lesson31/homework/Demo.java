8e2d252469952a16833cc110d28a53217d5b753c
Kilipurt <proandrey15@gmail.com>
2018-05-08 13:46:28 +0300

    lesson 31 & homework

--- src/lesson31/homework/Demo.java
+++ src/lesson31/homework/Demo.java
@@ -2,16 +2,69 @@ package lesson31.homework;
 
 public class Demo {
     public static void main(String[] args) {
-        String text = "erig erig reiuh 1erih 65erih ,erhui hier oiqu i uui";
+        String text1 = "";
+        String text2 = null;
+        String text3 = "s";
+        String text4 = "sss";
+        String text5 = "s456 ewgwg rw";
 
         try {
-            System.out.println(Solution.countSymbols(text));
+            System.out.println(Solution.countSymbols(text1));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
         try {
-            System.out.println(Solution.words(text));
+            System.out.println(Solution.countSymbols(text2));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.countSymbols(text3));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.countSymbols(text4));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.countSymbols(text5));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        try {
+            System.out.println(Solution.words(text1));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.words(text2));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.words(text3));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.words(text4));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(Solution.words(text5));
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
