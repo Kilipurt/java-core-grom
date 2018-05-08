e2118df1934e7b0ed011b7ac3283e06bb993d54b
Kilipurt <proandrey15@gmail.com>
2018-05-08 15:24:03 +0300

    lesson 32 & homework

---
+++ src/lesson32/ReadFromKeyboard.java
@@ -0,0 +1,59 @@
+package lesson32;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class ReadFromKeyboard {
+    public static void main(String[] args) {
+
+        try {
+            readKeyboardWithIOStream();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    private static void readKeyboardWithScanner() throws Exception {
+        //using scanner
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 2;
+
+        System.out.println("Please enter your name");
+
+        String name = scanner.nextLine();
+
+        validateName(name);
+
+        System.out.println("Hello, " + name);
+
+        scanner.close();
+    }
+
+    private static void readKeyboardWithIOStream() throws Exception {
+
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+
+        System.out.println("Please enter your name");
+
+        String name = br.readLine();
+
+        validateName(name);
+
+        System.out.println("Hello, " + name + "!");
+    }
+
+    private static void validateName(String name) throws Exception {
+        if (name == null || name.isEmpty())
+            throw new Exception("Wrong enter name");
+
+        for (char ch : name.toCharArray()) {
+            if (!Character.isLetter(ch)) {
+                throw new Exception("Name " + name + " has invalid characters");
+            }
+        }
+    }
+}
---
+++ src/lesson32/homework/Solution.java
@@ -0,0 +1,64 @@
+package lesson32.homework;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static int readNumbers() throws Exception {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+
+        int n = 2;
+        String[] numbers = null;
+
+        while (n >= 0) {
+            numbers = br.readLine().split(" ");
+
+            if (!validateNumbers(numbers)) {
+                if (n == 0) {
+                    System.out.println("Your numbers are wrong. Number of attempts exceeded");
+                    throw new Exception("Wrong enter number");
+                } else {
+                    System.out.println("Your numbers are wrong. You have " + n + " attempts to try again");
+                }
+            } else {
+                break;
+            }
+
+            n--;
+        }
+
+        return sumOfNumbers(numbers);
+    }
+
+    private static int sumOfNumbers(String[] numbers) {
+        int sum = 0;
+
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
+    }
+
+    private static boolean validateNumbers(String[] numbers) {
+        if (numbers == null || numbers.length != 10)
+            return false;
+
+        for (String number : numbers) {
+            if (number.isEmpty())
+                return false;
+
+            for (char ch : number.toCharArray()) {
+                if (!Character.isDigit(ch))
+                    return false;
+            }
+
+            if (Integer.parseInt(number) > 100)
+                return false;
+        }
+
+        return true;
+    }
+}
